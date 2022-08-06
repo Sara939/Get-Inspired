7d11e62dc26c8f8ff132508af5e431d736bcb506
sara-likaount <saralikaunt@gmail.com>
2022-08-06 23:02:29 +0300

    n

---
+++ quotes/author.js
@@ -0,0 +1,67 @@
+const searchString =  window.location.search;
+console.log('searchString: ', searchString);
+const urlParams = new URLSearchParams(searchString);
+console.log('urlParams: ', urlParams);
+const author = urlParams.get('author')
+console.log(author);
+
+let arrayOfQuotes;
+
+async function dataOnAuthor(){
+    try{
+        return await fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=10&exlimit=1&titles=${author}&explaintext=1&formatversion=2&format=json&origin=*`)
+
+        .then(res => res.json())
+        .then(data=>{
+            document.getElementById("data").innerHTML= `<div>${data.query.pages[0].extract}</div>`
+        })
+        // .then(json => console.log(json))
+    }
+    catch(err){
+        console.log(err);
+    }
+    finally{}
+}
+dataOnAuthor()
+
+
+async function imageOfAuthor(){
+    try{
+        return await fetch(`https://pixabay.com/api/?key=29100968-d97466929cf8d142ed8a99ce5&q=${author.split('_').join('+')}&image_type=photo`)
+
+        .then(res => res.json())
+        .then(data=>{
+            document.getElementById("image").innerHTML= `<img style= "width:200px; height:200px;" src= "${data.hits[0].largeImageURL}">`
+        })
+        // .then(json => console.log(json))
+    }
+    catch(err){
+        console.log(err);
+    }
+    finally{}
+}
+imageOfAuthor()
+
+
+async function getquotes(){
+    try{
+        return await fetch("https://type.fit/api/quotes")
+        .then(response => response.json())
+        .then(data => {
+            data = data.filter(a => a.author === author.split('_').join(' '))
+            
+            data.forEach(element => {
+                document.getElementById("id").innerHTML+=  `<div style="height:200px; width:100%;" class="carousel-item active">${element.text}</div>`
+                
+            });
+
+        });
+    }
+    catch(err){
+        console.log(err)
+    }
+    finally{}
+}
+
+//  getquotes();
+console.log(getquotes());
---
+++ quotes/quotes.js
@@ -0,0 +1,39 @@
+async function getquotes(){
+    try{
+        return await fetch("https://type.fit/api/quotes")
+        .then(response => response.json())
+    }
+    catch(err){
+        console.log(err)
+    }
+    finally{}
+}
+
+let arrayOfQuotes= [];
+onload= function buildarray(){ 
+    getquotes().then((response)=>{
+        // response.forEach(element => {
+        //         if (element.author!=null ) {
+        //             arrayOfQuotes.push(element);} // });
+        arrayOfQuotes = response.filter(element => element.author)
+        arrayOfAuthors = [];
+        // arrayOfQuotes.forEach(item => {
+        //     let i = arrayOfAuthors.findIndex(a => a.author === item.author);
+        //     if(i > -1 ){
+        //         arrayOfAuthors[i].quotes.push(item.text);
+        //     } else {
+        //         arrayOfAuthors.push({author: item.author,quotes:[item.text]})
+        //     }
+        // })
+        
+        console.log(arrayOfQuotes);
+        console.log(arrayOfAuthors);
+        arrayOfQuotes.slice(22,38).forEach(element => {
+            // console.log(element);
+            this.document.getElementById("cards").innerHTML+= `<div class="card" id="card" style="width: 18rem;"> <div class="card-body" id="card-body">
+            <h5 class="card-title">${element.author}</h5><p class="card-text">"${element.text}"</p><a href="/quotes/author.html?author=${element.author.split(' ').join('_')}" class="btn btn-primary" id="btn_card">Read More About ${element.author}</a> </div></div>`;
+    });
+    });
+}
+
+
