7d2fa95f3f7a02a68354ada6914f4faaf6905179
sara-likaount <saralikaunt@gmail.com>
2022-08-10 03:34:53 +0300

    n

--- Users/users.js
+++ Users/users.js
@@ -13,10 +13,10 @@ async function getUsersInfo() {
 
 
 
-// onload= function printUsersInfo() {
+// function printUsersInfo() {
 
 //     getUsersInfo().then((answer) => {answer.forEach(element => {
-//     users_table.innerHTML += `<tr scope="row" id="${element._id}">
+//     users_table.innerHTML += `<tr scope="row" >
 //     <td>${element.name.first} ${element.name.last}</td><td>${element.age}<td>${element.email}</td>
 //     </td><td>${element.phone}</td><td>
 //     <img style= "width: 6vw; " class="img-thumbnail" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80"/></td><td>
@@ -29,16 +29,13 @@ async function getUsersInfo() {
 onload= function printUsersInfo() {
 
     getUsersInfo().then((answer) => {answer.forEach(element => {
-    card.innerHTML +=  `<ul class="list-group "><li class="list-group-item">${element.name.first} ${element.name.last}</li>
+    card.innerHTML +=  `<ul id="${element._id}" class="list-group"><li class="list-group-item">${element.name.first} ${element.name.last}</li>
     <li class="list-group-item">${element.age}</li>
     <li class="list-group-item">${element.email}</li><li class="list-group-item">${element.phone}</li><button class="btn btn-danger" onclick="deletUser('${element._id}')">&#9940;</button></ul>`    
 
         });
     })
 }
-
-
-
 
 
 
