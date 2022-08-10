c48667b000f306541ec78663fa4e04d597092ae7
sara-likaount <saralikaunt@gmail.com>
2022-08-10 03:28:51 +0300

    nn

--- Users/users.js
+++ Users/users.js
@@ -13,19 +13,32 @@ async function getUsersInfo() {
 
 
 
+// onload= function printUsersInfo() {
+
+//     getUsersInfo().then((answer) => {answer.forEach(element => {
+//     users_table.innerHTML += `<tr scope="row" id="${element._id}">
+//     <td>${element.name.first} ${element.name.last}</td><td>${element.age}<td>${element.email}</td>
+//     </td><td>${element.phone}</td><td>
+//     <img style= "width: 6vw; " class="img-thumbnail" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80"/></td><td>
+//    <button class="btn btn-danger" onclick="deletUser('${element._id}')">&#9940;</button></td>
+//     </tr>
+//     `
+//         });
+//     })
+// }
 onload= function printUsersInfo() {
 
     getUsersInfo().then((answer) => {answer.forEach(element => {
-    users_table.innerHTML += `<tr scope="row" id="${element._id}">
-    <td>${element._id}</td><td>${element.age}</td><td>${element.name.first}</td><td>${element.name.last}</td><td>
-    ${element.email}</td><td>${element.index}</td><td>${element.phone}</td><td>
-    <img style= "width: 3vw; " class="img-thumbnail" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80"/></td><td>
-   <button class="btn btn-danger" onclick="deletUser('${element._id}')">&#9940;</button></td>
-    </tr>
-    `
+    card.innerHTML +=  `<ul class="list-group "><li class="list-group-item">${element.name.first} ${element.name.last}</li>
+    <li class="list-group-item">${element.age}</li>
+    <li class="list-group-item">${element.email}</li><li class="list-group-item">${element.phone}</li><button class="btn btn-danger" onclick="deletUser('${element._id}')">&#9940;</button></ul>`    
+
         });
     })
 }
+
+
+
 
 
 
@@ -40,3 +53,5 @@ async function deletUser(id){
     finally{}
 
 }
+
+
